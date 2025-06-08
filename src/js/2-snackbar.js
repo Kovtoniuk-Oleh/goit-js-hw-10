2603f735c036dc42fe9ebae1a696a1e3050c4e6c
Oleh_Kovtoniuk <kovtoniuk.oleh@gmail.com>
2025-06-08 15:45:57 +0200

    modified:   src/2-snackbar.html         modified:   src/css/styles.css         modified:   src/js/2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,7 +2,7 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const form = document.querySelector(".snackbar-form");
+const form = document.querySelector(".form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
